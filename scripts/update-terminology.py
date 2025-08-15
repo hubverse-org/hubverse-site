bff4c1ee97002206e642274568bd5bf5b380dd8e
Alvaro J. Castro Rivadeneira <56451982+micokoch@users.noreply.github.com>
2025-08-15 15:39:32 -0400

    one more curly brace missing

--- scripts/update-terminology.py
+++ scripts/update-terminology.py
@@ -24,7 +24,7 @@ def process_definitions(md):
     # 1. Transform "Modeling Tasks Terminology" section
     md = re.sub(
         r"\(model-tasks\)=\s*## Modeling Tasks Terminology\s*\n\[.*?Learn more about modeling tasks.*?\]\(.*?\)\s*",
-        f"## Modeling Tasks Terminology {{#model-tasks}}\n\n[{{{< fa book >}}} Learn more about modeling tasks]({MODEL_TASKS_URL}){{.btn .btn-outline-dark .ms-auto}}\n\n",
+        f"## Modeling Tasks Terminology {{#model-tasks}}\n\n[{{{{< fa book >}}}} Learn more about modeling tasks]({MODEL_TASKS_URL}){{.btn .btn-outline-dark .ms-auto}}\n\n",
         md,
         flags=re.DOTALL
     )
